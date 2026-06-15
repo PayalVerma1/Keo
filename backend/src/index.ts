8bf829229c37f0fa262659793359c566a7ee5413
PayalVerma1 <payalvermaseema@gmail.com>
2026-06-15 17:35:06 +0530

    Redis skeleton

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
 import { app } from "./app.ts";
 import { createServer } from "http";
 import { initSocketServer } from "./modules/websocket/socket.server.ts";
-
+import { connectRedis } from "./config/redis.ts";
 const port = process.env.PORT || 3000;
 
 const server = createServer(app);
 
 initSocketServer(server);
-
+await connectRedis();
 server.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
