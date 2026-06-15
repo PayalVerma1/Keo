9e44a156eae6121bd90eef025be528cb9e03a563
PayalVerma1 <payalvermaseema@gmail.com>
2026-06-16 02:04:06 +0530

    feat:redis producer

--- backend/src/streams/producers.ts
+++ backend/src/streams/producers.ts
@@ -1,1 +1,30 @@
-export {};
+import { connectRedis } from "../config/redis.ts";
+import {STREAMS} from "./redis-streams.ts";
+
+type StreamPayLoad = Record<string,unknown>;
+
+const addToStream = async(
+    stream:string,
+    payload:StreamPayLoad
+)=>{
+    return connectRedis.sendCommand([
+        "XADD",
+        stream,
+        "MAXLEN",
+        "~",
+        "10000",
+        "*",
+        "payload",
+        JSON.stringify(payload),
+    ])
+};
+export const Metric = async(payload:StreamPayLoad)=>{
+    return addToStream(STREAMS.METRICS,payload);
+};
+export const LOG = async(payload:StreamPayLoad)=>{
+    return addToStream(STREAMS.LOGS, payload);
+};
+
+export const publishDeployment = async (payload: StreamPayLoad)=> {
+  return addToStream(STREAMS.DEPLOYMENTS, payload);
+};
